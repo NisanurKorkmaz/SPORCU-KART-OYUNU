d4393403c23890200f731a98bf05314c0cdc6dd4
Nisanur Korkmaz <80275552+NisanurKorkmaz@users.noreply.github.com>
2021-12-09 14:08:04 +0300

    Update Test.java

--- SporcuKartOyunu/src/sporcugame/Test.java
+++ SporcuKartOyunu/src/sporcugame/Test.java
@@ -8,9 +8,7 @@ public class Test{
     
     public static void main(String[] args) {
         
-        
-    //ArrayList<String> sporcutakimlari = new ArrayList<>();
-    //ArrayList<String> sporcuisimleri = new ArrayList<>();
+
     ArrayList<Futbolcu> futbolcuListesi = new ArrayList<>();
     ArrayList<Basketbolcu> basketbolcuListesi = new ArrayList<>();
     Basketbolcu isil = new Basketbolcu("Işıl Alben", "Basketbol");
